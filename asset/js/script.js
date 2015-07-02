ae0ebf272c145a61502cc1478ef92cda6e6e2517
chin8628 <boon.8628@hotmail.com>
2015-07-03 00:32:56 +0700

    Add something

--- asset/js/script.js
+++ asset/js/script.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     var widthScreen = $( window ).width() - 200;
     var heightScreen = $( window ).height() - 150;
     var temp = "";
-    var count = 0;
+    var count = 140;
     var name = "cake"
     var intervalId;
     var hey = 0
@@ -17,15 +17,18 @@ $( document ).ready(function() {
         else if(count == 100) alert("WOW cake!");
         else if(count == 150){
             clearInterval(window.intervalId);
+            $( "h3").text("Click me plz Senpai!");
+            alert("Need your clicks for CAKE!");
             $('.btn-cake').fadeIn();
             name = "cake2";
             heightScreen -= 30;
-          widthScreen -= 30;
+            widthScreen -= 30;
         }
         else if(count == 200){
-          name = "doge";
-          heightScreen -= 50;
-          widthScreen -= 30;
+            $( "h3").text("WOW Click! much cake!");
+            name = "doge";
+            heightScreen -= 50;
+            widthScreen -= 30;
         }
         $( "section" ).append( "<img src='img/" + name + ".png' class='" + name + " a" + count + "' />" );
         $( ".countonme").text("Count: " + count);
